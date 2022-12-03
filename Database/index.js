906a80c8e95e7451e8f7299f7e343d50f2af1f4f
Meenu Katariya <102709273+MeenuKatariya@users.noreply.github.com>
2022-12-02 21:36:37 -0800

    Update index.js

--- Database/index.js
+++ Database/index.js
@@ -3,7 +3,8 @@ require('dotenv').config()
 
 async function connectDB() {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect(
+mongodb+srv://todoapp:todoapptodo@cluster0.oljlevu.mongodb.net/?retryWrites=true&w=majority);
         console.log('connected to database');
     } catch (error) {
         console.log(error);
